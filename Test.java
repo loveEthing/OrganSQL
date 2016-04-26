ad1692d22d1c8ae1b256b0f66374b08191c7a79d
loveEthing <838568619@qq.com>
2016-04-26 17:24:00 +0800

    简单测试程序

---
+++ Test.java
@@ -0,0 +1,15 @@
+```Java
+import com.zl.sql.ZData;
+import com.zl.sql.ZSelect;
+public class Test {
+	public static void main(String[] args) throws Exception {
+		String sql = "select x.a,y.b,c from x left join y on x.id=y.id";
+		ZSelect select = new ZSelect();
+		long start = System.currentTimeMillis();
+		ZData data = select.parseSelect(sql);
+		System.out.println(data);
+		long end = System.currentTimeMillis();
+		System.out.println("解析用时:"+(end-start)+"ms");
+	}
+}
+```
